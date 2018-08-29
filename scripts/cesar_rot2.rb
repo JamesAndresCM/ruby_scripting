f3a3e982e4ca79c4ad613433a34779da33656ba1
JamesACM <jaimecanalesmarin@gmail.com>
2018-08-28 22:03:14 -0300

    Update cesar_rot2.rb

--- scripts/cesar_rot2.rb
+++ scripts/cesar_rot2.rb
@@ -1,21 +1,21 @@
 #cifrado césar rot2
 def convert(n)
-  data = Array.new
-  letters = ('a'..'z').to_a
-  value = n.downcase.delete(', ').split('')
-  abort("#{value.join} error solo letras") unless value.join =~ /^[a-z]+$/
+	data = Array.new
+	letters = ('a'..'z').to_a
+	value = n.downcase.delete(', ').split('')
+	abort("#{value.join} error solo letras") unless value.join =~ /^[a-z]+$/
 	value.each do |pos| 
 		if pos.eql? 'z'
 			pos='a'
-    	res=letters.index(pos) + 1
+			res=letters.index(pos) + 1
 		elsif pos.eql? 'y'
 			pos='a'
- 		  res=letters.index(pos)
+			res=letters.index(pos)
 		else  
-		  res=letters.index(pos) + 2
+			res=letters.index(pos) + 2
 		end 
 		res_final=letters[res]
-    data << res_final
+		data << res_final
 	end
 	{input: n, output: data.join}
 end
