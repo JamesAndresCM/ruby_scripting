c294e3fba6bfb443bf85a4a1d8560ab53a98bbbe
JamesACM <jaimecanalesmarin@gmail.com>
2018-08-28 21:53:02 -0300

    fix sintax

--- scripts/cesar_rot2.rb
+++ scripts/cesar_rot2.rb
@@ -1,25 +1,25 @@
 #cifrado césar rot2
 def convert(n)
-data = Array.new
-abecedario = ('a'..'z').to_a
-	value = n.downcase.delete(', ').split("")
-  abort("#{value.join} error solo letras") unless value.join() =~ /^[a-z]+$/
+  data = Array.new
+  letters = ('a'..'z').to_a
+  value = n.downcase.delete(', ').split('')
+  abort("#{value.join} error solo letras") unless value.join =~ /^[a-z]+$/
 	value.each do |pos| 
-		if pos.eql? "z" 
-		  pos="a" 
-    	res=abecedario.index(pos) + 1 
-		elsif pos.eql? "y" 
-			pos="a" 
- 		  res=abecedario.index(pos) 
+		if pos.eql? 'z'
+		  pos='a'
+    	res=letters.index(pos) + 1
+		elsif pos.eql? 'y'
+			pos='a'
+ 		  res=letters.index(pos)
 		else  
-		  res=abecedario.index(pos) + 2 
+		  res=letters.index(pos) + 2
 		end 
-		res_final=abecedario[res] 
+		res_final=letters[res]
     data << res_final
 	end
-  final = {input: n, output: data.join}
+	{input: n, output: data.join}
 end
-puts convert("Carmelo")
-puts convert("Casa")
-puts convert("WIKIPEDIA, LA ENCICLOPEDIA LIBRE")
-puts convert("Cas5")
+puts convert('Carmelo')
+puts convert('Casa')
+puts convert('WIKIPEDIA, LA ENCICLOPEDIA LIBRE')
+puts convert('Cas5')
