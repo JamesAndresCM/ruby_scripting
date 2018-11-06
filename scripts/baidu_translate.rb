69b537feea0670b1ade79f4d5b3a4a20fffb5396
JamesACM <jaimecanalesmarin@gmail.com>
2018-11-06 05:04:02 -0300

    convert class to script with args via cli

--- scripts/baidu_translate.rb
+++ scripts/baidu_translate.rb
@@ -1,12 +1,23 @@
 require 'net/http'
 require 'uri'
 require 'json'
+require 'optparse'
 
 module SaltAuth
   require 'digest/md5'
   def auth(app_id, query, salt,secret_key)
     sign = "#{app_id}#{query}#{(salt.to_s)}#{secret_key}"
     Digest::MD5.hexdigest(sign)
+  end
+end
+
+class String
+  def colorize(color_code)
+    "\e[#{color_code}m#{self}\e[0m"
+  end
+
+  def red
+    colorize(31)
   end
 end
 
@@ -34,7 +45,7 @@ class BaiduTranslate
   end
 
   def language_args
-    %i[zh en yue wyw jp kor fra spa th ara ru pt de it el nl pl bul est dan fin cs rom slo swe hu cht]
+    %w[zh en yue wyw jp kor fra spa th ara ru pt de it el nl pl bul est dan fin cs rom slo swe hu cht]
   end
 
   private
@@ -47,7 +58,7 @@ class BaiduTranslate
 
     error = JSON.parse(response)["error_code"]
     if error == '54001' || error == '52003'
-      raise AuthError, 'Error Auth'
+      raise AuthError, 'Error Auth API BAIDU'.red
     else
       JSON.parse(response)["trans_result"][0]["dst"]
     end
@@ -56,14 +67,70 @@ end
 
 #Get your app_id!! http://api.fanyi.baidu.com/api/trans/product/index
 
-# Usage
-=begin
-a = BaiduTranslate.new(your_app_id,your_secret_key)
-p "Langs : #{a.language_args}"
-puts a.translate
-puts "\nConvert Auto to english"
-puts a.translate("spa","en","auto")
-=end
-  
-   
+translator = BaiduTranslate.new(your_app_id,your_secret_key)
+langs = translator.language_args
+
+#if args empty show -h
+ARGV << '-h' if ARGV.empty?
+
+options = {}
+
+optparse = OptionParser.new do |opts|
+  opts.banner = "Usage: #{$0} [options]"
+
+  opts.on('--from LANG', 'From Lang') do |f|
+    options[:from] = f
+    if options[:from].empty?
+      puts optparse
+      puts 'Param --from is required'.red
+      exit
+    elsif !langs.include?(options[:from])
+      puts optparse
+      puts "from: #{options[:from].red} not valid language"
+      exit
+    end
+  end
+
+  opts.on('--to LANG','Select Lang') do |t|
+    options[:to] = t
+    if options[:to].empty?
+      puts optparse
+      puts 'Param --to is required'.red
+      exit
+    elsif !langs.include?(options[:to])
+      puts optparse
+      puts "to: #{options[:to].red} not valid language"
+      exit
+    end
+  end
+  opts.on('--text TEXT', 'Text to translate') do |te|
+    options[:text] = te
+    if options[:text].empty?
+      puts optparse
+      puts 'Param --text is required'.red
+      exit
+    end
+  end
+  opts.on('-l','--lang', 'Display langs') do |l|
+    options[:lang] = l
+    puts optparse
+    puts "Langs : #{langs}" if options[:lang]
+    exit
+  end
+  opts.on( '-h', '--help', 'Display this screen' ){ puts opts; exit }
+end
+
+begin
+  optparse.parse!
+  if options[:from] and options[:to] and options[:text]
+    puts "\nText: #{options[:text]}\nFrom: #{options[:from].capitalize}\nTo: #{options[:to].capitalize}\n\nTranslate :#{translator.translate(options[:from],options[:to],options[:text])}"
+  else
+    puts optparse
+    exit
+  end
+  rescue StandardError => error
+    puts error
+    puts optparse
+    exit 2
+end
 
