5d51219369c33e2b6b0a00e78f9689fbbb65a3cc
JamesAndresCM <jaimecanalesmarin@gmail.com>
2019-05-23 21:57:23 -0400

    fix: refactor method and permit anyone range

--- scripts/slice_number_array.rb
+++ scripts/slice_number_array.rb
@@ -1,20 +1,12 @@
 class Array
   def slice_number_method(n)
-    return self if self.size <= 1 
-    not_in_arr = []
-    temp_arr = self.map do |ta| 
-      (1..ta).to_a.pop(ta - (ta-n)) if ta % n == 0
-    end.compact
-  
-    self.each do |last_num|
-      temp_arr << last_num if last_num > temp_arr.flatten.last
+    return self if self.size <= 1
+    self.each_with_object([]).each do |el, arr|
+      arr << self.slice!(0..(n-1))
+      arr << self if self.size.eql? 1
     end
-
-    temp_arr.each{|x|  not_in_arr << x unless x.is_a? self.class }
-    temp_arr.push(not_in_arr) unless not_in_arr.empty?
-    temp_arr.delete_if{|del| !del.is_a? self.class }
   end
 end
 
- range = (1..13).to_a
+ range = [1,23,45,1,23,4,5,12,3,12]
  p range.slice_number_method(3)
